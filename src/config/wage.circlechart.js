65dd8c2646804ef92a7b55191222a1bd243ad0cf
Ziyu Bai <49544681+coding-bai@users.noreply.github.com>
2020-11-27 19:37:39 +0800

    [feat]:完成工资详情

---
+++ src/config/wage.circlechart.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    type: "基本工资",
+    value: 3700,
+  },
+  {
+    type: "房补",
+    value: 2300,
+  },
+  {
+    type: "饭补",
+    value: 1000,
+  },
+  {
+    type: "提成",
+    value: 1500,
+  },
+  {
+    type: "奖金",
+    value: 1500,
+  },
+  {
+    type: "社保",
+    value: 1000
+  }
+];
---
+++ src/config/wage.line.js
@@ -0,0 +1,50 @@
+export default [
+	{
+		month: "1",
+		value: 2083,
+	},
+	{
+		month: "2",
+		value: 3084,
+	},
+	{
+		month: "3",
+		value: 2083.5,
+	},
+	{
+		month: "4",
+		value: 8085,
+	},
+	{
+		month: "5",
+		value: 6084.9,
+	},
+	{
+		month: "6",
+		value: 4086,
+	},
+	{
+		month: "7",
+		value: 2087,
+	},
+	{
+		month: "8",
+		value: 2089,
+	},
+	{
+		month: "9",
+		value: 2013,
+	},
+	{
+		month: "10",
+		value: 2087,
+	},
+	{
+		month: "11",
+		value: 2089,
+	},
+	{
+		month: "12",
+		value: 2013,
+	},
+];
