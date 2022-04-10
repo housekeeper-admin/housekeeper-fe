bafaba4ec38a5d4474c8b1d32be6616cdb18122a
baiziyu-fe <baiziyu-fe@outlook.com>
2022-04-10 11:10:54 +0800

    feat(proxy): server conf

--- vite.config.js
+++ vite.config.js
@@ -70,17 +70,7 @@ export default defineConfig({
     },
     server: {
         host: true,
-        port: 3096,
+        port: 8080,
         cors: true,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8096/',
-                // changeOrigin: true,
-                rewrite: (path) => path.replace(/\/api/, ''),
-                configure: (proxy, options) => {
-                    // proxy 是 'http-proxy' 的实例
-                }
-            }
-        }
     }
 })
